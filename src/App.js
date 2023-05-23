b1a4e57829706b507a7d19b161d4ffbf5bd294d6
phyerim <aamm2222@hanmail.net>
2023-05-24 03:00:25 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from './resource/bg.webp'
+import imgKakao from './resource/icKakao.svg'
+import imgLink from './resource/icLink.svg'
+import imgFacebook from './resource/icFacebook.svg'
+import imgTwitter from './resource/icTwitter.svg'
 
 function App() {
   return (
     <div className="App">
-      gg
+      <div className='reasoningLayout'>
+        <div className='main'>
+
+          <div className='mainShare'/>
+          <div className='mainTitle'>
+            당신의 많은것을<br/>
+            추리 해 보겠습니다
+          </div>
+
+          <div className='startButton'>
+            시작하기
+          </div>
+          <div className='mainShare'>
+            <img src={imgKakao} alt=''/>
+            <img src={imgLink} alt=''/>
+          </div>
+        </div>
+        <img src={bg} alt='' className='bg'/>
+      </div>
     </div>
   );
 }
